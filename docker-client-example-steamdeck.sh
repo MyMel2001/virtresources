718196263e20c2d3e0c7f57b0554494a36b15dd1
MyMel2001 Holder <163201376+mymel2001-holder@users.noreply.github.com>
2026-01-08 14:44:20 -0600

    decrease steamdeck ram

--- docker-client-example-steamdeck.sh
+++ docker-client-example-steamdeck.sh
@@ -1,1 +1,1 @@
-node index.js --connect $1:9000 240 --ram 8192 --gpu 2048 > docker-vr-client.log 2>&1 &  # big node
+node index.js --connect $1:9000 240 --ram 3192 --gpu 2048 > docker-vr-client.log 2>&1 &  # big node
